ce156915ea99220b39d65cc3e5161e059ed1a0b0
Waine Tam <wainetam@gmail.com>
2015-01-14 11:31:08 -0500

    Completed strings

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -17,14 +17,23 @@
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+    NSRange cheeseSuffixRange = [cheeseName rangeOfString:@" cheese" options:NSCaseInsensitiveSearch];
+    if(cheeseSuffixRange.location != NSNotFound) {
+        NSString *cheeseNameWithoutCheeseSuffix = [cheeseName stringByReplacingCharactersInRange:cheeseSuffixRange
+                                                                                  withString:@""];
+        return cheeseNameWithoutCheeseSuffix;
+    }
+    return cheeseName;
+//    return nil;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        return [NSString stringWithFormat:@"%d cheese", (int)cheeseCount];
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        return [NSString stringWithFormat:@"%d cheeses", (int)cheeseCount];
     }
     
     /*
