8ade043aa3c81d94dec8bed945f24425cb043e08
Waine Tam <wainetam@gmail.com>
2015-01-17 16:29:32 -0500

    Completed dictionaries

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -9,20 +9,52 @@
 #import "StarTrekDictionaries.h"
 
 @implementation StarTrekDictionaries
+//{
+//    "name": "Nog",
+//    "rank": "Lieutenant junior grade",
+//    "information": "the first Ferengi to enter Starfleet",
+//    "favorite drink": "root beer",
+//    "quote": "Hear all, trust nothing."
+//}
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    
+    id faveDrink = [characterDictionary objectForKey:@"favorite drink"];
+    
+    if(faveDrink) {
+        return faveDrink;
+    }
+    
+    return nil;
+//    return @"";
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
     /* WORK HERE */
-    return @[];
+    
+//    NSMutableArray *drinks = [NSMutableArray array];
+    NSMutableArray *drinks = [@[] mutableCopy];
+    
+    for (NSDictionary *character in charactersArray) {
+        if (character[@"favorite drink"]) {
+            [drinks addObject:character[@"favorite drink"]];
+        }
+    }
+    
+    return (NSArray *)drinks;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @{};
+    
+    NSMutableDictionary *newDict = [characterDictionary mutableCopy];
+    
+    NSString *quote = @"It's like gogurt to stay";
+    
+//    newDict[@"quote"] = quote;
+    [newDict setObject:quote forKey:@"quote"];
+    
+    return newDict;
 }
-
 @end
